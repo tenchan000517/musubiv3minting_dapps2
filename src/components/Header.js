b46524de063e4dd8e84f9b706626859815d20190
tenchan000517 <tenchan@nft-mint.xyz>
2024-01-19 17:35:54 +0900

    header navi animation

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,8 +5,22 @@ function Header() {
   return (
     <header className="app-header">
       <div className="container">
-        <img src="/logo192.png" alt="kurimaro logo" className="logo" />
-        <h1>kurimaro collection</h1>
+          
+          <div className='logo-container'>
+          <img src="/logo192.png" alt="kurimaro logo" className="logo" />
+          </div>
+
+          <div className="navi-container">
+            <img src="/navi1.png" alt="Navi 1" className="navi-image"/>
+            <img src="/navi2.png" alt="Navi 2" className="navi-image"/>
+            <img src="/navi3.png" alt="Navi 3" className="navi-image"/>
+            <img src="/navi4.png" alt="Navi 4" className="navi-image"/>
+          </div>
+
+          <div className="header-content">
+          <h1>kurimaro collection</h1>
+          </div>
+
       </div>
     </header>
   );
